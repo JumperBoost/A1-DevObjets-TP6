1231477f54b78904e737f37f7d10d358468b8945
Petru Valicov <petru.valicov@umontpellier.fr>
2022-03-03 09:45:54 +0100

    trouverTrou() visibilité par paquetage

--- src/main/java/fr/umontpellier/iut/partie1/Taquin.java
+++ src/main/java/fr/umontpellier/iut/partie1/Taquin.java
@@ -18,7 +18,7 @@ public class Taquin {
     }
 
     // retourne un tableau [i,j] si tableau[i][j]==0
-    public int[] trouverTrou() {
+    int[] trouverTrou() {
         throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
     }
 
