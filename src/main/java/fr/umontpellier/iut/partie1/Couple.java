8625bfc4f3096ffde4e4e7bf6e568fe4d1f9a949
Petru Valicov <petru.valicov@umontpellier.fr>
2023-03-06 13:12:26 +0100

    commentaires Couple

--- src/main/java/fr/umontpellier/iut/partie1/Couple.java
+++ src/main/java/fr/umontpellier/iut/partie1/Couple.java
@@ -12,10 +12,18 @@ public class Couple {
         this.predecesseur = predecesseur;
     }
 
+    /**
+     * Vérifie si les fils du taquin sont déjà vus et met à jour la frontière
+     * et l'ensemble des configurations déjà vues.
+     */
     public void mettreAJour(ArrayList<Couple> frontiere, ArrayList<Taquin> dejaVus) {
         throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
     }
 
+    /**
+     * @return la liste des taquins intermédiaire à partir du taquin initial
+     * et jusqu'au taquin courant
+     */
     public ArrayList<Taquin> getListeDeMouvements() {
         throw new RuntimeException("Méthode non implémentée ! Effacez cette ligne et écrivez le code nécessaire");
     }
