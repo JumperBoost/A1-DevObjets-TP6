a0e82dd301b5845f610fc19c12d736d99a66e8ed
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-03-19 13:41:35 +0000

    Ajout de la spécification 'public' dans 'JeuPuzzle'

--- src/main/java/fr/umontpellier/iut/partie2/JeuPuzzle.java
+++ src/main/java/fr/umontpellier/iut/partie2/JeuPuzzle.java
@@ -3,6 +3,6 @@ package fr.umontpellier.iut.partie2;
 import java.util.ArrayList;
 
 public interface JeuPuzzle {
-    boolean estGagnant();
-    ArrayList<?> genererFils();
+    public boolean estGagnant();
+    public ArrayList<?> genererFils();
 }
